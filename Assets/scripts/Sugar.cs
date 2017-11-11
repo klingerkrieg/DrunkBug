c4f727c0dacffb55d2471a5b609b493c4d4a331d
Alan Klinger <klingerkrieg@gmail.com>
2017-11-11 16:18:11 -0300

    Sugars melhorados

--- Assets/scripts/Sugar.cs
+++ Assets/scripts/Sugar.cs
@@ -6,6 +6,43 @@ public class Sugar : MonoBehaviour {
 
 	public int sugar = 5;
 
+
+	public float FadeDuration = 1f;
+	public Color Color1 = Color.gray;
+	public Color Color2 = Color.white;
+
+	private Color startColor;
+	private Color endColor;
+	private float lastColorChangeTime;
+
+	private Material material;
+
+
+
+	void Start() {
+		material = GetComponent<Renderer>().material;
+		startColor = Color1;
+		endColor = Color2;
+	}
+
+	void Update()
+	{
+		var ratio = (Time.time - lastColorChangeTime) / FadeDuration;
+		ratio = Mathf.Clamp01(ratio);
+		material.color = Color.Lerp(startColor, endColor, ratio);
+		//material.color = Color.Lerp(startColor, endColor, Mathf.Sqrt(ratio)); // A cool effect
+		//material.color = Color.Lerp(startColor, endColor, ratio * ratio); // Another cool effect
+
+		if (ratio == 1f) {
+			lastColorChangeTime = Time.time;
+
+			// Switch colors
+			var temp = startColor;
+			startColor = endColor;
+			endColor = temp;
+		}
+	}
+
 	void OnCollisionEnter (Collision col) {
 		col.gameObject.GetComponent<Player> ().addSugar(sugar);
 		Destroy (this.gameObject);
